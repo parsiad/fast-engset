f3caf8479bd6ae4d48d3c3b05655fc07e288189c
parsiad <parsiad.azimzadeh@gmail.com>
2015-07-08 11:02:42 -0400

    fe.py: removed dependency on scipy.

--- fast_engset/fe.py
+++ fast_engset/fe.py
@@ -3,7 +3,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2015 Parsiad Azimsadeh and Tommy Carpenter>
+Copyright (c) 2015 Parsiad Azimzadeh and Tommy Carpenter
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -12,25 +12,22 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-   THE SOFTWARE.
-"""
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
-"""
 Computes the blocking probability of a finite population queue.
-
 
 Example usage:
 
-import fast_engset as fe
+from fast_engset import fe
 
 m = 5  # Number of servers
 N = 10 # Number of sources
@@ -66,11 +63,57 @@ def compute(m, N, E, tol=1e-6):
     if N <= m: return 0
     if m == 0: return 1
 
-    return __quasi_newton(m, N, E, tol=tol)
+    return __newton(m, N, E, tol=tol)
 
 ################################################################################
 
-def __quasi_newton(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
+def __hyp2f1_coefficients(m, N):
+    """ Precomputes the coefficients of 1/f(P) = hyp2f1(1, -m, N-m, z).
+
+    m -- a positive integer.
+    N -- a positive integer greater than m.
+    """
+
+    f = m
+    g = N-m
+    c = [0]*(m+1)
+    c[0] = 1.
+    k = 1
+    while True:
+        c[k] = f/g * c[k-1]
+        f -= 1
+        if f == 0: break
+        k += 1
+        g += 1
+
+    return c
+
+def __hyp2f1(c, m, N, E, x, tol):
+    """ Computes 1/f(P) = hyp2f1(1, -m, N-m, -x).
+
+    c   -- coefficients computed by fe.__coefficients.
+    m   -- a positive integer.
+    N   -- a positive integer greater than m.
+    E   -- a real number.
+    x   -- a real number.
+    tol -- error tolerance.
+    """
+
+    h1 = 1.
+    mlt = x
+    k = 1
+    while True:
+        u = c[k] * mlt
+        h1 += u
+        if k == m or abs(u/h1) <= tol: break
+        k += 1
+        mlt *= x
+
+    return h1
+
+################################################################################
+
+def __newton(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
     """ Computes the blocking probability of a finite population queue as given
     by the Engset formula using a quasi-Newton's method.
 
@@ -89,41 +132,15 @@ def __quasi_newton(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
                otherwise, only P is returned (default False).
     """
 
-    # Precompute coefficients of 1/f(P) = hyp2f1(1, -m, N-m, 1-N/E-P)
-    f = m
-    g = N-m
-    c = [0]*(m+1)
-    c[0] = 1.
-    k = 1
-    while True:
-        c[k] = f/g * c[k-1]
-        f -= 1
-        if f == 0: break
-        k += 1
-        g += 1
+    c = __hyp2f1_coefficients(m, N)
 
     y = N/E-1
     for n in range(1, n_max+1):
-        # Compute f(P) and f(P + dP)
-        x    = P+y
-        h1   = 1.
-        h1e  = 1.
-        mlt  = x
-        mlte = x+tol
-        k = 1
-        while True:
-            u  = c[k] * mlt
-            ue = c[k] * mlte
-            h1  += u
-            h1e += ue
-            if k == m or (abs(u/h1) <= tol and abs(ue/h1e) <= tol): break
-            k += 1
-            mlt  *= x
-            mlte *= x+tol
+        x = P+y
+        f = 1/__hyp2f1(c, m, N, E, x, tol)
+        g = 1/__hyp2f1(c, m, N, E, x+tol, tol)
 
-        # Newton iteration
-        recip = 1/h1
-        P_new = P + (recip - P)/( (recip - 1/h1e)/tol + 1 )
+        P_new = P + (f - P)/( (f - g)/tol + 1 )
 
         if abs(P - P_new) <= tol:
             if verbose: return (P_new, n)
@@ -133,46 +150,16 @@ def __quasi_newton(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
 
     return None
 
-def __newton(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
-    """ Computes the blocking probability of a finite population queue as given
-    by the Engset formula using Newton's method.
-
-    For a list of parameters, see fast_engset.quasi_newton.
-    """
-
-    from scipy.special import hyp2f1
-
-    b = -m
-    c = N-m
-    d = 1-N/E
-
-    for n in range(1, n_max+1):
-        h1 = hyp2f1(1, b  , c  , d-P)
-        h2 = hyp2f1(2, b+1, c+1, d-P)
-
-        P_new = P + ( 1/h1 - P )/( m/c * h2/(h1*h1) + 1 )
-
-        if abs(P - P_new) <= tol:
-            if verbose: return (P_new, n)
-            return P_new
-
-        P = P_new
-
-    return None
-
-def __bisect(m, N, E, tol=pow(2,-24), n_max=1024, verbose=False):
+def __bisection(m, N, E, tol=pow(2,-24), n_max=1024, verbose=False):
     """ Computes the blocking probability of a finite population queue as given
     by the Engset formula using bisection.
 
-    For a list of parameters, see fast_engset.quasi_newton.
+    For a list of parameters, see fe.newton.
     """
 
-    from scipy.special import hyp2f1
+    c = __hyp2f1_coefficients(m, N)
 
-    b = -m
-    c = N-m
-    d = 1-N/E
-
+    y = N/E-1
     lo = 0
     hi = 1
     for n in range(1, n_max+1):
@@ -182,24 +169,21 @@ def __bisect(m, N, E, tol=pow(2,-24), n_max=1024, verbose=False):
             if verbose: return (P, n)
             return P
 
-        if 1/hyp2f1(1, b, c, d-P) < P: hi = P
-        else:                          lo = P
+        if 1/__hyp2f1(c, m, N, E, P+y, tol) < P: hi = P
+        else:                                  lo = P
 
 def __fixed_point(m, N, E, tol=pow(2,-24), P=0.5, n_max=1024, verbose=False):
     """ Computes the blocking probability of a finite population queue as given
     by the Engset formula using a fixed point iteration.
 
-    For a list of parameters, see fast_engset.quasi_newton.
+    For a list of parameters, see fe.newton.
     """
 
-    from scipy.special import hyp2f1
+    c = __hyp2f1_coefficients(m, N)
 
-    b = -m
-    c = N-m
-    d = 1-N/E
-
+    y = N/E-1
     for n in range(1, n_max+1):
-        P_new = 1/hyp2f1(1, b, c, d-P)
+        P_new = 1/__hyp2f1(c, m, N, E, P+y, tol)
 
         if abs(P - P_new) <= tol:
             if verbose: return (P_new, n)
