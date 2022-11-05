255640125018aa6637126f08252e8643a881bb7f
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2022-11-05 18:59:54 -0400

    Add type hints and tox action.

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
+"""setup.py"""
+
 import re
 
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as handle:
+with open("README.md", "r", encoding="utf8") as handle:
     lines = handle.readlines()
 
 # Remove links from TOC since they aren't supported on PyPI
-start = lines.index('## Table of contents\n') + 1
-end = lines.index('## 📔 Citation\n')
+start = lines.index("## Table of contents\n") + 1
+end = lines.index("## 📔 Citation\n")
 for i in range(start, end):
-    lines[i] = re.sub(r'\[([^]]+)\]\([^)]+\)', r'\1', lines[i])
+    lines[i] = re.sub(r"\[([^]]+)\]\([^)]+\)", r"\1", lines[i])
 
-long_description = ''.join(lines)
+long_description = "".join(lines)
 
 setup(
-    name='fast-engset',
-    author='Parsiad Azimzadeh, Tommy Carpenter',
-    author_email='parsiad.azimzadeh@gmail.com',
-    description=('Fast and accurate routines to compute various quantities in the Engset model.'),
-    license='MIT',
+    name="fast-engset",
+    author="Parsiad Azimzadeh, Tommy Carpenter",
+    author_email="parsiad.azimzadeh@gmail.com",
+    description=("Fast and accurate routines to compute various quantities in the Engset model."),
+    extras_require={"JIT": ["numba"]},
+    license="MIT",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=['enum;python_version<"3"', 'numba;python_version>="3"', 'numpy'],
+    long_description_content_type="text/markdown",
+    install_requires=["numpy"],
     packages=find_packages(),
-    url='https://github.com/parsiad/fast-engset',
-    version='3.0.1',
+    url="https://github.com/parsiad/fast-engset",
+    version="3.0.1",
 )
